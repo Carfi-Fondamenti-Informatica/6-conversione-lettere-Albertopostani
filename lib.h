de1467a226f7d79772ab9b3380e4a1cf505e41e4
Albertopostani <91824639+Albertopostani@users.noreply.github.com>
2021-12-01 19:56:26 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,6 +1,7 @@
-#ifndef LIB_LIB_H
-#define LIB_LIB_H
+#ifndef RICORSIONE_LIB_H
+#define RICORSIONE_LIB_H
 
 bool primo(char a);
 char trasformazione(char a);
-#endif //LIB_LIB_H
+
+#endif //RICORSIONE_LIB_H
